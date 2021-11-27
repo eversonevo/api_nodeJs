7834eceeefa224c9f51b3a306eb64be9550bb098
Evo Desenvolvimentos <evotreinamentos@gmail.com>
2021-11-26 22:41:10 -0300

    ate email

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-global.SALF_KEY = '';
-global.EMAIL_TMPL = '<strong>{0}<strong>';
+global.SALF_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}<strong>, seja bem vindo!';
 
 module.exports = {
     conectionString: "mongodb+srv://evodesenvolvimento:evo12345@cluster0.ci9n6.mongodb.net",
-    sendgridKey: 'TDB',
-    containerConnectionString: 'TDB'
+    sendgridKey: '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',
+    containerConnectionString: 'TBD'
 }
--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -5,6 +5,9 @@
 
 const ValidationContract = require('../validators/fluent_validator');
 const repository = require('../repositories/customer-repository');
+const md5 = require('md5');
+
+const emailService = require('../services/email_service');
 
 exports.post = async(req,res,next) => {
 
@@ -19,7 +22,13 @@ exports.post = async(req,res,next) => {
     }
 
     try {
-        await repository.create(req.body);
+        await repository.create({
+            name:req.body.name,
+            email: req.body.email,
+            password:md5(req.body.password+global.SALT_KEY)
+        });
+
+        emailService.send(req.body.email,'Bem vindo',global.EMAIL_TMPL.replace('{0},req.body.name'));
         res.status(201).send({message:'Cliente Cadastrado com sucesso'});
     
     } catch (error) {
